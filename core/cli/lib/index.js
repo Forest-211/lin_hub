9b19c9207b1535218b5bff7cf0753293830fc247
changlin93 <767425412@qq.com>
2021-04-06 16:13:53 +0800

    :sparkles: 检查node版本

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,14 +1,36 @@
 'use strict'
-
-const { info, success, notice } = require('@lin-hub/log')
+const semver = require('semver')
+const colors = require('colors/safe')
+const { info, error, notice } = require('@lin-hub/log')
 const pkg = require('../package.json')
-
-module.exports = core
+const constant = require('./constant')
 
 function core() {
-    checkPkgVersion()
+    try {
+        checkPkgVersion()
+        checkNodeVersion()
+    } catch (err) {
+        error(err.message)
+    }
 }
 
 function checkPkgVersion() {
     notice(pkg.version)
 }
+
+function checkNodeVersion() {
+    /**
+     * Tasking
+     * - 获取当前node版本号 process.version
+     * - 比对最低版本号
+     */
+    const currentVersion = process.version
+    const lowestVersion = constant.LOWEST_NODE_VERSION
+    if (!semver.gte(currentVersion, lowestVersion)) {
+        throw new Error(
+            colors.red(`lin-hub 需要安装v${lowestVersion}以上版本的Node.js`)
+        )
+    }
+}
+
+module.exports = core
